c7ff07168868f4a2d3ff4753370660250c004d73
Timo Zimmermann <timo@screamingatmyscreen.com>
2018-06-19 11:20:56 +0200

    Admin command and async deploy
    Add admin command to deploy website
    Add database backed queue to deploy website

--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,35 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
+from django_q.tasks import async
+
 from .models import Log, Configuration
+from .tasks import deploy
+
+
+def publish(modeladmin, request, queryset):
+    async(deploy)
+
+
+publish.short_description = "publish website"
+
+
+class ConfigurationAdmin(admin.ModelAdmin):
+    actions = (publish, )
+
+    def changelist_view(self, request, extra_context=None):
+        """Select first configuration item for publish to allow running the
+        command without requiring a selection.
+
+        This needs to be removed for a multi-site setup.
+        """
+        if "action" in request.POST and request.POST["action"] == "publish":
+            post = request.POST.copy()
+            cfg = Configuration.objects.first()
+            post.update({admin.ACTION_CHECKBOX_NAME: str(cfg.id)})
+            request._set_post(post)
+        return super().changelist_view(request, extra_context)
 
 
 admin.site.register(Log)
-admin.site.register(Configuration)
+admin.site.register(Configuration, ConfigurationAdmin)
--- website/tasks.py
+++ website/tasks.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
-from django_q.tasks import async
+from .models import Configuration
+from entities.models import Entity
+from design.models import Template
 
-from .models import Log
+from drupan.engine import Engine
 
 
 def deploy():
-    async(__deploy)
+    config = Configuration.as_config()
+    templates = Template.as_dict()
+    entities = Entity.as_list(config)
 
+    engine = Engine()
+    engine.site.templates = templates
+    engine.site.entities = entities
+    engine.config = config
 
-def __deploy():
-    Log.objects.create(success=True, output="foobar")
+    engine.prepare_engine()
+    engine.run()
