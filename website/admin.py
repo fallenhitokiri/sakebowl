92aba01b39be8aa0ba7c3282a48570b8ecfcc562
Timo Zimmermann <timo@screamingatmyscreen.com>
2018-10-06 16:03:41 +0200

    Update async call for most recent django-q version

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from django_q.tasks import async
+from django_q.tasks import async_task
 
 from .models import Log, Configuration
 from .tasks import deploy
@@ -9,7 +9,7 @@ from .tasks import deploy
 
 def publish(modeladmin, request, queryset):
     for cfg in queryset.all():
-        async(deploy, cfg.id)
+        async_task(deploy, cfg.id)
 
 
 publish.short_description = "publish website"
